9660be454e49600ad2666c5ea4e05a3ff861634f
Kashish Jain <jkashish33@gmail.com>
2025-09-17 00:11:11 -0400

    Package update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -16,25 +16,18 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTabsModule } from '@angular/material/tabs';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    OptionsPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    MatButtonModule,
-    MatCardModule,
-    MatInputModule,
-    MatSelectModule,
-    MatFormFieldModule,
-    MatTabsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        OptionsPageComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        MatButtonModule,
+        MatCardModule,
+        MatInputModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        MatTabsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
